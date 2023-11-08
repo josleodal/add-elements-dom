c42384b55e72685b2e689c17158baa6949519e3e
Jose Miguel Leon <josleodal@gmail.com>
2023-11-08 18:29:34 +0100

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
  // Aquí tu código
+const boton = document.getElementById('agregar');
+
+
+
+boton.addEventListener("click", function(){
+
+
+let hijo = document.createElement("li");
+let elemento =document.createTextNode(prompt("Agregue un enlemento", ""));
+texto= document.getElementById("lista").appendChild(hijo);
+texto.appendChild(elemento);
+
+
+
+
+});
